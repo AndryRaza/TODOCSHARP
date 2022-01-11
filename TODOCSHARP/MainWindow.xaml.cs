6cf25e8c63c1528168f3ec9055cda5963bd30b69
AndryRaza <razaandry974@gmail.com>
2022-01-11 15:45:01 +0400

    Ajout dictionnaire

--- TODOCSHARP/MainWindow.xaml.cs
+++ TODOCSHARP/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Data.Sqlite;
 
 namespace TODOCSHARP
 {
@@ -23,6 +24,98 @@ namespace TODOCSHARP
         public MainWindow()
         {
             InitializeComponent();
+            getTasks();
+        }
+
+        public List<String> nameTask = new List<string> { };
+        public List<String> descriptionTask = new List<string> { };
+
+        public class Task{
+            private string name;
+            private string description; 
+            public Task(string n, string d)
+            {
+                name = n; 
+                description = d;
+            }
+        }
+
+        public Dictionary<int,Task> tasks = new Dictionary<int,Task>(); 
+
+        private void getTasks()
+        {
+            string pathDB = @"Data Source=C:\Users\widoo\Desktop\sqlitestudio-3.3.3\SQLiteStudio\todo";
+            using (var connection = new SqliteConnection(pathDB))
+            {
+                connection.Open();
+                var query =
+                    @"
+                        SELECT *
+                        FROM task
+                    ";
+
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+
+ 
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var name= reader.GetString(1);
+                        var description = reader.GetString(2);
+                        nameTask.Add(name); 
+                        descriptionTask.Add(description);
+                        tasks.Add(Int32.Parse(reader.GetString(0)),new Task(reader.GetString(1),reader.GetString(2)));
+                    }
+       
+                }
+                showTasks();
+            }
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            addtask addtask = new addtask();
+            this.Close();
+            addtask.ShowDialog();
+        }
+
+        private void showTasks()
+        {
+            var rowGroup = contentTable.RowGroups.FirstOrDefault();
+
+            foreach(var task in tasks)
+            {
+                Console.WriteLine(task.Value);
+            }
+
+            if (rowGroup != null)
+            {
+                int i = 0;
+                while(i < nameTask.Count)
+                {
+                    TableRow row = new TableRow();
+
+                    TableCell cell = new TableCell();
+
+                    cell.Blocks.Add(new Paragraph(new Run(nameTask[i])));
+                    row.Cells.Add(cell);
+
+                    cell = new TableCell();
+                    cell.Blocks.Add(new Paragraph(new Run(descriptionTask[i])));
+                    row.Cells.Add(cell);
+
+                    rowGroup.Rows.Add(row);
+                    i++;
+                }
+            }           
+        }
+
+        private void editTask(int i)
+        {
+            string pathDB = @"Data Source=C:\Users\widoo\Desktop\sqlitestudio-3.3.3\SQLiteStudio\todo";
         }
     }
 }
---
+++ TODOCSHARP/addtask.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Microsoft.Data.Sqlite;
+
+namespace TODOCSHARP
+{
+    /// <summary>
+    /// Logique d'interaction pour addtask.xaml
+    /// </summary>
+    public partial class addtask : Window
+    {
+        public addtask()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_add_Click(object sender, RoutedEventArgs e)
+        {
+            string pathDB = @"Data Source=C:\Users\widoo\Desktop\sqlitestudio-3.3.3\SQLiteStudio\todo";
+            using (var connection = new SqliteConnection(pathDB))
+            {
+                connection.Open();
+                var query =
+                    @"
+                        INSERT INTO task
+                        (name,description,status)
+                        VALUES ($name,$description,true)
+                    ";
+
+                var command = connection.CreateCommand();
+                command.CommandText = query;
+                command.Parameters.AddWithValue("$name", task.Text);
+                command.Parameters.AddWithValue("$description", description.Text);
+                command.ExecuteNonQuery();
+
+                MainWindow mainWindow = new MainWindow();
+                this.Close();
+                mainWindow.Show();
+            }
+        }
+    }
+}
